7673210cb64b6743725bf779043a74836f4b70ef
keffri <keffri.w.neal@gmail.com>
2022-07-26 08:48:58 -0700

    imported Section component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navigation from "./components/Navigation";
+import Section from "./components/Section";
 
 function App() {
   return (
     <main className="App">
       <Navigation />
+      <Section title="Home" />
     </main>
   );
 }
