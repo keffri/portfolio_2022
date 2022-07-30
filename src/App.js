82229df25919a46abbce8fb59c89d54843da6a7c
keffri <keffri.w.neal@gmail.com>
2022-07-30 11:08:17 -0700

    components imported + added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <main className="App">
       <Navigation />
-      <Home title="Home" />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
     </main>
   );
 }
