d5d73aab054c4a414b2c7b85968755d8dfb607f4
keffri <keffri.w.neal@gmail.com>
2022-07-30 11:07:26 -0700

    project(s) components template

---
+++ src/components/Projects/Project.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Project = (props) => {
+  return (
+    <section className="project">
+      <img
+        className="project__img"
+        src={props.src}
+        alt={`${props.projectNo}`}
+      />
+      <section className="project__info">
+        <h1 className="project__title">{props.name}</h1>
+        <p className="project__desc">{props.desc}</p>
+        <div className="project__icons">
+          {props.icons.map((element) => {
+            return <img src={element} alt="icon" className="project__icon" />;
+          })}
+        </div>
+        <div className="project__buttons">
+          <button className="project__button">
+            <a href={props.liveDemo} target="_blank" rel="noreferrer noopener">
+              Live Demo
+            </a>
+          </button>
+          <button className="project__button">
+            <a href={props.viewCode} target="_blank" rel="noreferrer noopener">
+              View Code
+            </a>
+          </button>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Project;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Project from "./Project";
+import wwp from "../../images/wheresWaldoPreview.png";
+
+const Projects = () => {
+  const devIcons = [
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+  ];
+
+  const projectInfo = [
+    {
+      name: "Where's Waldo",
+      src: wwp,
+      desc: "A Where's Waldo project built using React, and Firebase to verify coordinates of a mouse click against the stored coordinates of a selected character.",
+      liveDemo: "https://keffri.github.io/wheres-waldo/",
+      viewCode: "https://github.com/keffri/wheres-waldo",
+    },
+  ];
+
+  return (
+    <section className="projects">
+      <h1 className="projects__title">Projects</h1>
+      <Project
+        projectNo="1"
+        name={projectInfo[0].name}
+        src={projectInfo[0].src}
+        desc={projectInfo[0].desc}
+        icons={[
+          devIcons[0],
+          devIcons[1],
+          devIcons[2],
+          devIcons[3],
+          devIcons[4],
+        ]}
+        liveDemo={projectInfo[0].liveDemo}
+        viewCode={projectInfo[0].viewCode}
+      />
+    </section>
+  );
+};
+
+export default Projects;
