5d6c0fca4c0576f73e028a43b7817342b4887242
keffri <keffri.w.neal@gmail.com>
2022-08-03 09:45:33 -0700

    Contact component completion

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,48 @@ const Contact = () => {
   return (
     <section className="contact">
       <h1 className="contact__title">Contact</h1>
+      <form
+        action="https://formsubmit.co/keffri.w.neal@gmail.com"
+        method="POST"
+        className="contact__form"
+      >
+        <label className="contact__label" htmlFor="name">
+          Name:
+        </label>
+        <input type="text" id="name" name="name" placeholder="Name" required />
+        <label className="contact__label" htmlFor="email">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+        <label className="contact__label" htmlFor="subject">
+          Subject:
+        </label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          placeholder="Subject"
+          required
+        />
+        <label className="contact__label" htmlFor="message">
+          Message:
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Message..."
+          cols="30"
+          rows="10"
+        ></textarea>
+
+        <input type="submit" value="Send" className="contact__submit" />
+      </form>
     </section>
   );
 };
