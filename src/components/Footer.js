d657861d76f2e2a5b50232d411b97a848f1f9c16
keffri <keffri.w.neal@gmail.com>
2022-08-03 14:47:49 -0700

    Footer component completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const Footer = () => {
   return (
-    <footer>
-      <h1>footer</h1>
+    <footer className="footer">
+      <h3 className="footer__title">Keffri Neal</h3>
+      <section className="footer__links">
+        <a
+          href="https://www.linkedin.com/in/keffri/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FontAwesomeIcon icon={brands("linkedin")} className="footer__icon" />
+        </a>
+        <a
+          href="https://github.com/keffri"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FontAwesomeIcon icon={brands("github")} className="footer__icon" />
+        </a>
+        <a
+          href="https://twitter.com/keffri"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FontAwesomeIcon icon={brands("twitter")} className="footer__icon" />
+        </a>
+      </section>
     </footer>
   );
 };
