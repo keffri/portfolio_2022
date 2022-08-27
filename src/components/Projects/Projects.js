19020a2090420a55879553eded0e87831dda7bab
keffri <keffri.w.neal@gmail.com>
2022-08-27 11:03:36 -0700

    added Portfolio project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Project from "./Project";
 import wwp from "../../images/wheresWaldoPreview.png";
+import pp from "../../images/portfolio2022Preview.png";
 
 const Projects = () => {
   const devIcons = [
@@ -9,6 +10,8 @@ const Projects = () => {
     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
   ];
 
   const projectInfo = [
@@ -19,6 +22,14 @@ const Projects = () => {
       desc: "A Where's Waldo project built using React, and Firebase to verify coordinates of a mouse click against the stored coordinates of a selected character.",
       liveDemo: "https://keffri.github.io/wheres-waldo/",
       viewCode: "https://github.com/keffri/wheres-waldo",
+    },
+    {
+      id: "projects/3.2",
+      name: "Personal Portfolio",
+      src: pp,
+      desc: "My current portfolio built with React as well as React-Bootstrap and Sass for front-end styling and functionality.",
+      liveDemo: null,
+      viewCode: "https://github.com/keffri/portfolio_2022",
     },
   ];
 
@@ -39,6 +50,22 @@ const Projects = () => {
         ]}
         liveDemo={projectInfo[0].liveDemo}
         viewCode={projectInfo[0].viewCode}
+      />
+      <Project
+        id={projectInfo[1].id}
+        name={projectInfo[1].name}
+        src={projectInfo[1].src}
+        desc={projectInfo[1].desc}
+        icons={[
+          devIcons[0],
+          devIcons[1],
+          devIcons[2],
+          devIcons[3],
+          devIcons[5],
+          devIcons[6],
+        ]}
+        liveDemo={projectInfo[1].liveDemo}
+        viewCode={projectInfo[1].viewCode}
       />
     </section>
   );
