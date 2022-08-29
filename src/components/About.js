d6fd7a8ce9f4a8c3c7eafe9fc33703f4d6710ac2
keffri <keffri.w.neal@gmail.com>
2022-08-29 10:27:43 -0700

    updated text

--- src/components/About.js
+++ src/components/About.js
@@ -8,20 +8,20 @@ const About = () => {
         <div className="about__intro">
           <p className="about__text">
             My name is Keffri Neal and I am a self-taught web developer from
-            Port Coquitlam, Canada. I started to teach myself web development at
-            the beginning of COVID quarantine back in 2020, and I have been
-            continuing to learn ever since. When it comes to coding I enjoy
+            Port Coquitlam, Canada. I began teaching myself web development at
+            the beginning of COVID-19 quarantine back in March 2020, and I have
+            been continuing to learn ever since. When it comes to coding I enjoy
             problem solving, being creative and getting to see web pages come to
             life.
           </p>
           <p className="about__text">
             I graduated from the University of Kentucky with a B.A. in
             Psychology. I attended on a full athletic scholarship as a track and
-            cross country runner for the four years I was a student.
+            cross country runner for the four years that I attended.
           </p>
           <p className="about__text">
-            When I am not coding I enjoy lifting, cycling, basketball, reading
-            and cooking.
+            When I am not coding I enjoy spending my free time exercising,
+            reading, drawing and cooking.
           </p>
         </div>
         <div className="about__carousel"></div>
