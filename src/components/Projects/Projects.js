346a0e1009577e4673d35ea847b08700e7b6fd02
keffri <keffri.w.neal@gmail.com>
2022-09-10 13:40:46 -0700

    added mini message board data

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,35 +1,46 @@
-import React from "react";
-import Project from "./Project";
-import wwp from "../../images/wheresWaldoPreview.png";
-import pp from "../../images/portfolio2022Preview.png";
+import React from 'react';
+import Project from './Project';
+import wwp from '../../images/wheresWaldoPreview.png';
+import pp from '../../images/portfolio2022Preview.png';
+import mmb from '../../images/miniMessageBoardPreview.png';
 
 const Projects = () => {
   const devIcons = [
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg',
   ];
 
   const projectInfo = [
     {
-      id: "projects/3.1",
+      id: 'projects/3.1',
       name: "Where's Waldo",
       src: wwp,
       desc: "A Where's Waldo project built using React that utilizes Firebase to store the coordinates a character which is then used to verify the character's location clicks on coordinates of the web page.",
-      liveDemo: "https://keffri.github.io/wheres-waldo/",
-      viewCode: "https://github.com/keffri/wheres-waldo",
+      liveDemo: 'https://keffri.github.io/wheres-waldo/',
+      viewCode: 'https://github.com/keffri/wheres-waldo',
     },
     {
-      id: "projects/3.2",
-      name: "Personal Portfolio",
+      id: 'projects/3.2',
+      name: 'Personal Portfolio',
       src: pp,
-      desc: "My current portfolio built with React as well as React-Bootstrap and Sass for front-end styling and functionality.",
+      desc: 'My current portfolio built with React as well as React-Bootstrap and Sass for front-end styling and functionality.',
       liveDemo: null,
-      viewCode: "https://github.com/keffri/portfolio_2022",
+      viewCode: 'https://github.com/keffri/portfolio_2022',
+    },
+    {
+      id: 'projects/3.3',
+      name: 'Mini Message Board',
+      src: mmb,
+      desc: 'A Mini Message Board built using NodeJS, Express and Pug. New messages data requested from the /new path with post is used to update the messages on the index page.',
+      liveDemo: 'https://blooming-earth-14314.herokuapp.com/',
+      viewCode: 'https://github.com/keffri/mini-message-board',
     },
   ];
 
@@ -66,6 +77,21 @@ const Projects = () => {
         ]}
         liveDemo={projectInfo[1].liveDemo}
         viewCode={projectInfo[1].viewCode}
+      />
+      <Project
+        id={projectInfo[2].id}
+        name={projectInfo[2].name}
+        src={projectInfo[2].src}
+        desc={projectInfo[2].desc}
+        icons={[
+          devIcons[0],
+          devIcons[1],
+          devIcons[2],
+          devIcons[7],
+          devIcons[8],
+        ]}
+        liveDemo={projectInfo[2].liveDemo}
+        viewCode={projectInfo[2].viewCode}
       />
     </section>
   );
