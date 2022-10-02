70b390dc5ccb2788af9c13769203fa0798411ccf
keffri <keffri.w.neal@gmail.com>
2022-10-02 13:59:59 -0700

    updated resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
               className="home__icon"
             />
             <a
-              href="https://github.com/keffri/portfolio_2022/blob/d85aeb3f6039d71f2770fb2466db9ef108257dd7/src/files/Keffri-Neal-Resume.pdf"
+              href="https://github.com/keffri/portfolio_2022/blob/master/src/files/Keffri_Neal_Resume.pdf"
               rel="noreferrer"
               target="_blank"
             >
