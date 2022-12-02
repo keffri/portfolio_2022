59dbbf8f1746a2dd973a616b14dce7821e20bc69
keffri <keffri.w.neal@gmail.com>
2022-12-02 14:31:13 -0800

    updated with post-css logo

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import rtl from '../images/rtl-logo.png';
+import pcss from '../images/postcss-logo.svg';
 
 const Skills = () => {
   return (
@@ -590,6 +591,12 @@ const Skills = () => {
               fill="#38b2ac"
             ></path>
           </svg>
+          <img
+            src={pcss}
+            alt="post-css logo"
+            className="skills__svg"
+            title="PostCSS"
+          ></img>
         </div>
       </section>
     </section>
