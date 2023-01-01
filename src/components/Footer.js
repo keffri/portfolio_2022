d2bb312b28431a5ca1bbfe37677f16decce8fc8a
keffri <keffri.w.neal@gmail.com>
2022-12-31 18:28:12 -0800

    added resume to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { brands } from "@fortawesome/fontawesome-svg-core/import.macro";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  brands,
+  regular,
+} from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Footer = () => {
   return (
@@ -12,14 +15,24 @@ const Footer = () => {
           target="_blank"
           rel="noreferrer noopener"
         >
-          <FontAwesomeIcon icon={brands("linkedin")} className="footer__icon" />
+          <FontAwesomeIcon icon={brands('linkedin')} className="footer__icon" />
         </a>
         <a
           href="https://github.com/keffri"
           target="_blank"
           rel="noreferrer noopener"
         >
-          <FontAwesomeIcon icon={brands("github")} className="footer__icon" />
+          <FontAwesomeIcon icon={brands('github')} className="footer__icon" />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1sgmx9hBdSu-g13lq-FQ1stH-FC2KEbcE/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FontAwesomeIcon
+            icon={regular('file-lines')}
+            className="footer__icon"
+          />
         </a>
       </section>
     </footer>
