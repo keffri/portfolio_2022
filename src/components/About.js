5cfa380a819990097b595a488a816a842738092d
keffri <keffri.w.neal@gmail.com>
2023-05-04 18:19:13 -0700

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,21 +7,28 @@ const About = () => {
       <section className="about__container">
         <div className="about__intro">
           <p className="about__text">
-            My name is Keffri Neal and I am a web developer from Port Coquitlam,
-            B.C. I started learning web development in March of 2020, and I have
-            been continuing to learn and improve ever since. When it comes to
-            coding I enjoy problem solving, being creative and getting to see
-            web pages come to life.
+            Hello there, I am Keffri Neal, a web developer based in Port
+            Coquitlam, British Columbia. I began my journey in web development
+            in March of 2020, and ever since then, I have been continuously
+            expanding my knowledge and improving my skills in this exciting
+            field. What drives my passion for coding is the satisfaction I get
+            from problem-solving, exploring my creativity and seeing web pages
+            come to life.
           </p>
           <p className="about__text">
-            I graduated from the University of Kentucky with a B.A. in
-            Psychology. I attended the university on a full athletic scholarship
-            as a track and cross country runner for the four years that I
-            attended.
+            After four years of pursuing my passion for running track and cross
+            country, I proudly graduated from the University of Kentucky with a
+            Bachelor of Arts degree in Psychology. Throughout my time at the
+            university, I was fortunate to be on a full athletic scholarship,
+            allowing me to excel both academically and athletically.
           </p>
           <p className="about__text">
-            When I am not coding I enjoy spending my free time exercising,
-            reading, writing and cooking.
+            In my spare time, I find fulfillment in a variety of hobbies.
+            Running and calisthenics keep me active and energized, because
+            maintaining my physical and mental wellbeing is a priority.
+            Additionally, I love to unwind with a good book, jot down my
+            thoughts and reflections in a journal, and experiment with new
+            recipes in the kitchen.
           </p>
         </div>
         <div className="about__carousel"></div>
